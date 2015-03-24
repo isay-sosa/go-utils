68d46af7483f3be7ccdba67bea4ebf5531ea59f2
Oscar Swanros <swanros@gmail.com>
2015-03-24 12:17:11 -0600

    Create utils package

---
+++ utils/utils.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"reflect"
+	"errors"
+)
+
+func NewError(e string) error {
+	if e != "" {
+		return errors.New(e)
+	}
+
+	return nil
+}
+
+func IsSlice(i interface{}) bool {
+	ival := ValueOf(i)
+	return ival.Kind() == reflect.Slice
+}
+
+func ValueOf(i interface{}) reflect.Value {
+	return reflect.ValueOf(i)
+}
